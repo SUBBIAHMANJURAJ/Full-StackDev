d457876755041ddf7b41450cc6c455dfc697f97f
SUBBIAHMANJURAJ <ssubbiahmanjuraj@gmail.com>
2026-02-20 14:13:02 +0530

    Switch case is added

---
+++ FrontEnd/javascript/switchCase.js
@@ -0,0 +1,14 @@
+let signal="Yellow";
+switch(signal){
+    case "red":
+        console.log("Stop");
+        break;
+    case "Yellow":
+        console.log("Be Ready");
+        break;
+    case "green":
+        console.log("Go");
+        break;
+        
+        
+}
