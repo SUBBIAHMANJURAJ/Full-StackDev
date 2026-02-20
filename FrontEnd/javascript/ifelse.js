80627c9cb05f4a947414ac4a50f60473c3bdd618
SUBBIAHMANJURAJ <ssubbiahmanjuraj@gmail.com>
2026-02-20 14:18:57 +0530

    ifelse elseif ladder added

---
+++ FrontEnd/javascript/ifelse.js
@@ -0,0 +1,38 @@
+// IF STATEMENT
+
+let a=18;
+if(a>=18){
+    console.log("vote");
+    
+}
+
+// IF ELSE STATEMENT
+
+let number=18;
+if(number%2==0){
+    console.log("even");
+    
+}else{
+    console.log("Odd");
+    
+}
+
+// ELSE IF LADDER
+
+let mark=76;
+if(mark>=90 && mark<=100){
+    console.log("Grade A");
+    
+}
+else if(mark>=75 && mark<=89){
+    console.log("Grade B");
+    
+}
+else if(mark>=50 && mark<=74){
+    console.log("Grade C");
+    
+}
+else{
+    console.log("Fail")
+    
+}
