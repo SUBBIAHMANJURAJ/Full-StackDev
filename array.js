ccd33e887adb6827bcc1eb7fe6dc6aa8a6cbfcba
SUBBIAHMANJURAJ <ssubbiahmanjuraj@gmail.com>
2026-02-21 10:58:00 +0530

    Add array manipulation examples with push, pop, and for loop

---
+++ array.js
@@ -0,0 +1,23 @@
+// PUSH
+
+let arr=[1,2,3,4,5,6,7]
+arr.push(8,9)
+console.log(arr);
+
+// POP
+
+let arr1=[1,2,3,4,5,6]
+arr1.pop()
+console.log(arr1);
+
+
+// ARRAY WITH FOR LOOP
+
+let arr2=[1,2,3,4,5,6,7,8,9,10]
+for(i=0;i<arr2.length;i++){
+    if(arr2[i]% 2==0){
+        console.log(arr2[i]);
+        
+    }
+
+}
