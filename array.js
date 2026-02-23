cd84334f5ebcc5d216e243fbcafd9599e3e73e85
SUBBIAHMANJURAJ <ssubbiahmanjuraj@gmail.com>
2026-02-23 09:26:22 +0530

    Update index.html to include array.js script and enhance array.js with additional examples with methods

--- array.js
+++ array.js
@@ -21,3 +21,58 @@ for(i=0;i<arr2.length;i++){
     }
 
 }
+
+for(i=0;i<50;i++){
+    if(i%2==0 && i%3==0){
+        console.log(i);
+        
+    }
+}
+
+
+// Array Hydration
+
+// SPLICE METHOD
+
+let arrr=["Manju","Giffy","Sudalai","Spartan"]
+arrr.splice(2,1,"Vijay")
+console.log(arrr);
+
+let arp=[1,2,4,5,6,7,9]
+arp.splice(2,0,3)
+arp.splice(7,0,8)
+console.log(arp);
+
+// PUSH METHOD
+
+// ADD NUMBER OR STRING AT THE LAST
+
+let arpush=[1,2,3,4,5,6,7]
+arpush.push(8)
+console.log(arpush);
+
+// ADD NUMBER OR STRING AT THE LAST
+
+let arpop=[2,4,6,8,10,12]
+arpop.pop()
+console.log(arpop);
+
+// SHIFT METHOD USED TO REMOVE FIRST ELEMENT
+
+let arshift=[13,1,2,3,4,5,6,7,8,9,10]
+arshift.shift()
+console.log(arshift);
+
+
+// UNSHIFT METHOD USED TO REMOVE FIRST ELEMENT
+
+let arunshift=[2,4,6,8,10,12]
+arunshift.unshift(0)
+console.log(arunshift);
+
+
+
+
+
+
+
