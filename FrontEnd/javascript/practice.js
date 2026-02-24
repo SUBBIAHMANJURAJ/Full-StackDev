3be33390f878d022423e4b0777e406685d00b657
SUBBIAHMANJURAJ <ssubbiahmanjuraj@gmail.com>
2026-02-24 09:55:53 +0530

    array methods push,pop,shift,unshift,splice is learned

---
+++ FrontEnd/javascript/practice.js
@@ -0,0 +1,77 @@
+let arr0=[5,10,15]
+arr0.splice(1,1,20)
+console.log(arr0);
+
+let arr1=[1,2,3,4,5]
+arr1.push(6,7)
+console.log(arr1);
+
+let arr2=[10,20,30]
+arr2.unshift(99)
+console.log(arr2);
+
+let arr3=[2,4,6,8,10]
+arr3.pop()
+console.log(arr3);
+
+let arr4=[3,6,9,12,15]
+arr4.splice(2,1,0)
+console.log(arr4);
+
+let arr5=[1,2,3,4,5]
+arr5.splice(arr5.length -1,1,10)
+console.log(arr5);
+
+let arr6=[1,2,3,4,5]
+arr6.unshift(8,9,10)
+console.log(arr6);
+
+let arr7=[10,20]
+arr7.push(25,30)
+console.log(arr7);
+
+let arr8=[300,400,500]
+arr8.unshift(100,200)
+console.log(arr8);
+
+let arr9=[1,2,3,4,5,6]
+arr9.push(7)
+console.log(arr9);
+
+let arr10=[11,22,33,44,55]
+arr10.splice(0,arr10.length)
+console.log(arr10);
+
+let arr11=[7,14,21]
+arr11.splice(0,1,10)
+console.log(arr11);
+
+let arr12=[3,4,5]
+arr12.push(2,1)
+console.log(arr12);
+
+let arr13=[50,40,30,20,10]
+arr12.shift()
+console.log(arr12);
+
+let arr14=[3,6,9,12,15]
+arr14.splice(3,1,7)
+console.log(arr14);
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
